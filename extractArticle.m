627290602d2135dc6ab4afc065962433908f3535
Royce Branning <royce5branning@gmail.com>
2015-04-13 14:42:33 -0400

    now it should work
    gets article and some extra stuff still

--- extractArticle.m
+++ extractArticle.m
@@ -1,7 +1,7 @@
 function x=extractArticle(yo)
 
 y=webread(yo);
-pattern='<?/?.*?>';
+pattern='</?.*?>';
 almost_article=regexprep(y,pattern,'');
 next_pattern='(?window.*\+\+r';
 x=regexprep(almost_article,next_pattern,'');
