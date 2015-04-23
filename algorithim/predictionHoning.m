e7207950ae9f51d1d0404e57b5c9c7952a6c11e6
Michael Krumdick <mrkrumdick@gmail.com>
2015-04-22 20:27:18 -0400

    Update predictionHoning.m

--- algorithim/predictionHoning.m
+++ algorithim/predictionHoning.m
@@ -1,34 +1,38 @@
 %% algoryhtim honing grounds, genetic methods
 
 clear
-%% Authorize
-Quandl.auth('a6WhbJvyBoVA_Czi9oFw');
-%% Generate data
-data = Quandl.get('BCHAIN/MKPRU','collapse','secondly');
-ts = data;
-price = data.Data;
+%% Getting Data
+priceText = fileread('bitcoinPriceData.txt');
+priceText = strsplit(priceText, '\n');
+for d = 1: 365
+    price(d) = str2num(char(priceText(d)));
+end
+
 totalDifference = 0;
 minDif = -1;
 j = 0;
 
-sentimentText = fileread('FakeData.txt');
+sentimentText = fileread('2014Sentiment.txt');
 sentimentText = strsplit(sentimentText, '\n');
 
 for y = 1:365
     split = strsplit(char(sentimentText(y)), 'e');
-    firstPart = str2num(char(split(1)));
-    secondPart = str2num(char(split(2)));  
-    sentiment(y) = firstPart * 10.^(secondPart);
+    if (length(split) > 1)
+        firstPart = str2num(char(split(1)));
+        secondPart = str2num(char(split(2)));  
+        sentiment(y) = firstPart * 10.^(secondPart);
+    end
+        sentiment(y) = str2num(char(sentimentText(y)));
 end
 
 %% test
-for t = 2.1
-    for c = -.7
-        for h = 0:.1:2
+for t = 1:10
+    for c = 1:10
+        for h = -5:-1
             if t ~= 0
             totalDifference = 0;
-                for i = 1:356
-                    totalDifference = totalDifference + abs(price(1833 + i) - predictPriceHone(sentiment(i), price(1832 + i), t, c, h));
+                for i = 1:364
+                    totalDifference = totalDifference + abs(price(i+1) - predictPriceHone(sentiment(i), price(i), t, c, h));
                 end
                 averageDif = totalDifference/365.0;
                 if minDif == -1
@@ -46,3 +50,7 @@ for t = 2.1
         end
     end
 end
+
+
+
+
